d1ae3b598232998d5c83e89ff54ac66102ba5702
Taylor Huston <taylor.a.huston@gmail.com>
2016-02-05 20:50:28 -0700

    Set up some basic routes for the 'admin' section

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+//Admin section
+var adminRouter = express.Router();
+
+//Admin main page
+adminRouter.get('/', function(req, res) {
+  res.send("Dashboard");
+});
+
+//Users
+adminRouter.get('/users', function(req, res) {
+  res.send("Users");
+});
+
+//Posts
+adminRouter.get('/posts',function(req, res) {
+  res.send("Posts")
+});
+
+app.use('/admin', adminRouter);
+
 //Start server
 app.listen(1337);
 console.log('1337 is the magic port!');
