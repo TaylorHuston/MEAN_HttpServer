93508731bd54f25fd24b46408609ac26b1562191
Taylor Huston <taylor.a.huston@gmail.com>
2016-02-24 23:23:55 -0700

    Set up some basic routes

--- server.js
+++ server.js
@@ -34,6 +34,26 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+ '/index.html'));
 });
 
+//Routes for Admin section
+var adminRouter = express.Router();
+
+//Admin main page. /admin
+adminRouter.get('/', function(req, res) {
+  res.send('Dashboard');
+});
+
+//Admin main page. /admin/users
+adminRouter.get('/users', function(req, res) {
+  res.send('Users');
+});
+
+//Admin main page. /admin/posts
+adminRouter.get('/posts', function(req, res) {
+  res.send('Posts');
+});
+
+app.use('/admin', adminRouter);
+
 //Start server
 app.listen(1337);
 console.log('Alive at localhost:1337');
