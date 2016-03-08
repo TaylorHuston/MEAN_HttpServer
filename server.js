78b02ff6905855e651f6fe6a9a0949cd431c05cc
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-07 19:46:53 -0700

    Adding to master repo

--- server.js
+++ server.js
@@ -1,69 +1,99 @@
-//Load the espress package and create our app
+var mongoose =require('mongoose');
+mongoose.connect('mongodb://localhost/http_server');
+
+//*****Pure Node*****
+
+////Get the HTTP and FIlesysten modules
+//var http = require('http'),
+//    fs = require('fs');
+//
+////Create Server
+//http.createServer(function (req, res) {
+//  res.writeHead(200, {
+//    'Content-Type': 'text/html',
+//    'Access-Control-Allow-Origin': '*'
+//  });
+//
+//  //Grab the index.html file
+//  var readStream = fs.createReadStream(__dirname + '/index.html');
+//
+//  //Send index file to browser
+//  readStream.pipe(res);
+//
+//
+//}).listen(1337);
+//
+//console.log('Alive at localhost:1337');
+
+//*****Using Express*****
+
+//Load Express
 var express = require('express');
 var app = express();
 var path = require('path');
-var mongoose = require('mongoose');
 
-
-//Connect to DB
-mongoose.connect('mongodb://localhost/db_name');
-
-//Send the index.html file to the user
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+//Send index.html to browser
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+ '/index.html'));
 });
 
-//Admin section using express.Router();
+//Routes for Admin section
 var adminRouter = express.Router();
 
-//Middleware that runs before any of the follow adminRouter actions
-adminRouter.use(function (req, res, next) {
+//Log each request
+adminRouter.use(function(req, res, next) {
   console.log(req.method, req.url);
   next();
+})
+
+//Admin main page. /admin
+adminRouter.get('/', function(req, res) {
+  res.send('Dashboard');
 });
 
-//Admin main page
-adminRouter.get('/', function (req, res) {
-  res.send("Dashboard");
+//Admin users page. /admin/users
+adminRouter.get('/users', function(req, res) {
+  res.send('Users');
 });
 
-//Users
-adminRouter.get('/users', function (req, res) {
-  res.send("Users");
+//Admin user page with params. /admin/users/:name
+adminRouter.get('/users/:name', function(req, res){
+  res.send('Hello '+req.params.name);
 });
 
-//Hello
-adminRouter.param('name', function (req, res, next, name) {
+//Admin hello page with 'validation'. /hello/:name
+adminRouter.param('name', function(req, res, next, name) {
   //Some validation
-  console.log("Validating " + name);
+  console.log("Validating: "  +name);
+  
   req.name = name;
   next();
 });
 
-adminRouter.get('/hello/:name', function (req, res) {
-  res.send("Hello " + req.params.name + "!");
-});
+adminRouter.get('/hello/:name', function(req,res) {
+  res.send('Hello ' + req.name);
+})
 
-//Posts
-adminRouter.get('/posts', function (req, res) {
-  res.send("Posts")
+
+//Admin posts page. /admin/posts
+adminRouter.get('/posts', function(req, res) {
+  res.send('Posts');
 });
 
 app.use('/admin', adminRouter);
 
-
-//Login using app.route()
+//Shorthand routing
 app.route('/login')
-  .get(function (req, res) {
-    res.send("Login Form")
+  //Login form. /login
+  .get(function(req, res) {
+    res.send("Login form");
   })
-  .post(function (req, res) {
-    console.log("validating login");
-    res.send("Processing");
-  });
-
-
+  //Process form. /login
+  .post(function(req, res) {
+  console.log('Login');
+  res.send("Logged In");
+});
 
 //Start server
 app.listen(1337);
-console.log('1337 is the magic port!');
+console.log('Alive at localhost:1337');
